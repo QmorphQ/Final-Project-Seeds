f1f42f635c718f8b0f680ecab7004ecacadc1622
serhiybondarenkogithub <93479567+serhiybondarenkogithub@users.noreply.github.com>
2022-01-29 00:19:15 +0200

    Revert "Revert 14 our products/tabs"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector , useDispatch } from "react-redux";
+import {fetchProducts} from "./store/thunks/products.thunks";
+import {
+  downloadRequestStateSelector,
+  productsSelector,
+} from "./store/selectors/selectors";
+import Home from "./app/pages/Home.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  const downloadRequestState = useSelector(downloadRequestStateSelector);
+  const productList = useSelector(productsSelector);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts("/Products.json"));
+  }, []);
+
+  return (
+    <div>
+      <Home 
+        loading={downloadRequestState} 
+        productList={productList} 
+      />
+    </div>
+  );
 }
 export default App;

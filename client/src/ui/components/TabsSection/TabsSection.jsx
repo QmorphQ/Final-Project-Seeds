3c904d7e78c19168ababd73b4328d86f038f771b
dariamolchanova18 <molchanovadariia13@gmail.com>
2022-01-28 23:20:20 +0200

    fixed our product section tubs

--- client/src/ui/components/TabsSection/TabsSection.jsx
+++ client/src/ui/components/TabsSection/TabsSection.jsx
@@ -1,18 +1,28 @@
+/* eslint-disable  */
 import React from 'react';
 import { makeStyles } from "@mui/styles";
-import { Typography } from '@mui/material';
+import { Typography, Container, Grid } from '@mui/material';
 import Tab from "../Tab/Tab.jsx"; 
 import Tabs from "../Tabs/Tabs.jsx"; 
 import Icon from "../Icon/Icon.jsx"; 
 
-/* eslint-disable  */
 const useStyles = makeStyles(() => ({
-        ourProducts: {
-            maxWidth: "1110px",   
+        ourProductsContainer: {
+            display: "flex",
+            flexDirection: "column",
         },
+
         ourProductHeading: {
             marginBottom: "40px !important",
             fontWeight: "bold !important",
+        },
+
+        ourProductTabsContainer: {
+            display: "flex",
+            justifyContent: "center"
+        },
+        ourProductTabs: {
+            marginBottom: "40px !important",
         }
     
 }))
@@ -66,19 +76,17 @@ const categoriies = [
 
     return (
         <>
-        <main>
-            <section className={classes.ourProducts}>
-                <div>
-                    <Typography className={classes.ourProductHeading} variant="h1" component="h2">
+            <Container className={classes.ourProducts}>
+                <Grid className={classes.ourProductsContainer}>
+                    <Typography className={classes.ourProductHeading} variant="h3" component="h3">
                         Our products.
                     </Typography>
-                    <div>
-                        <Tabs value={value} onChange={handleChange}>{categoriesTabs}</Tabs>
+                    <div className={classes.ourProductTabsContainer}>
+                        <Tabs className={classes.ourProductTabs} value={value} onChange={handleChange}>{categoriesTabs}</Tabs>
                     </div>
 
-                </div>
-            </section>
-        </main>
+                </Grid>
+            </Container>
         </>
     )
 }

702a48253dae942b606f5828c6eed47d601233da
avk.me <avk21.me@gmail.com>
2022-01-29 22:51:59 +0200

    fixed index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./store/store";
-import TestStore from "./store/TestStore.jsx";
 import CustomThemeProvider from "./ui/hoc/CustomThemeProvider.jsx";
 
 ReactDOM.render(
   <Provider store={store}>
     <CustomThemeProvider>
-      <TestStore/>
+      <App/>
     </CustomThemeProvider>
   </Provider>,
   document.getElementById("root")

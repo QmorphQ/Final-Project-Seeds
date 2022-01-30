868b286deab2c095a155f3af090bd378b00925e2
Serhiy Bondarenko <sergiogetstudy@gmail.com>
2022-01-30 03:39:34 +0200

    fix ui

--- client/src/ui/components/ProductCard/ProductCard.jsx
+++ client/src/ui/components/ProductCard/ProductCard.jsx
@@ -2,48 +2,120 @@ import { Card, CardActions, CardContent, CardHeader, CardMedia, Grid, IconButton
 import PropTypes from 'prop-types';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { ThemeProvider, useTheme } from "@mui/material/styles";
+import productCardTheme from "./productCardTheme";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+  productCard: {
+    borderRadius: "8px",
+    position:"relative", 
+    border: "2px solid #EFEFEF", 
+    boxShadow:"none", 
+    minHeight:"512px"
+  },
+  productCardHeader: {
+    position: "absolute", 
+    right:"0" 
+  },
+  productCardButton: {
+    border: "1px solid #EFEFEF"
+  },
+  productCardMedia: {
+    width: "294px", 
+    margin: "28px auto", 
+    borderRadius: "12px"  
+  },
+  productCardRating: {
+    margin: "0px 28px"
+  },
+  productCardContent: {
+    margin: "10px 28px", 
+    padding:"0"
+  },
+  productCardName: {
+    margin: "0px", 
+    height:"50px", 
+    overflow:"hidden"
+  },
+  productCardPrice: {
+    margin: "0px", 
+    lineHeight: "54px", 
+    fontWeight:"bold"
+  },
+  productCardButtonBasket: {
+    position: "absolute", 
+    bottom:"28px", 
+    right:"28px", 
+    borderRadius:"8px", 
+    width:"48px", 
+    height:"48px",
+    border: "1px solid #EFEFEF"
+  }
+}))
 
 const ProductCard = ({ product }) => {
-  const {name, currentPrice, categories, imageUrls} = product; /* eslint-disable-line*/
-  const localPrice = Intl.NumberFormat("us", {style: "currency", currency: "USD"});
-return (
-  <Grid item xs={12} md={6} lg={4}>
-    <Card sx={{position:"relative", border: "2px solid #EFEFEF", borderRadius: "8px", boxShadow:"none", minHeight:"512px"}}>
-      <CardHeader sx={{ position: "absolute", top:"0", right:"0" }}
-        action={
-          <IconButton aria-label="settings" sx={{border: "1px solid #EFEFEF", color:"#FFCF55"}}>
-            <FavoriteBorderIcon />
-          </IconButton>
-        }
-      />
-      <CardMedia
-        sx={{width: "294px", margin: "28px auto", borderRadius: "12px"}}
-        component="img"
-        width="294px"
-        image={`${imageUrls}`}
-        alt="SEEDRA 600 English Lavender Seeds for Indoor and Outdoor Planting"
-      />
-      <Rating
-        sx={{margin: "0px 28px"}}
-        name="half-rating" defaultValue={2.5} precision={0.5}
-        onChange={f => f}
-      />
-      <CardContent sx={{margin: "10px 28px", padding:"0"}}>
-        <Typography sx={{margin: "0px", height:"50px", overflow:"hidden"}} align="left" variant="body1" color="text.secondary">
-          {name}
-        </Typography>
-        <Typography sx={{margin: "0px", lineHeight: "54px", fontWeight:"bold"}} align="left" variant="h5" color="text.secondary">
-          {localPrice.format(currentPrice)}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton aria-label="add to favorites" sx={{ position: "absolute", bottom:"28px", right:"28px", border: "1px solid #EFEFEF", color:"#359740", borderRadius:"8px", width:"48px", height:"48px" }}>
-          <ShoppingCartIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-  </Grid>
-)
+  const {name, currentPrice, imageUrls} = product;
+  const globalTheme = useTheme();
+  const classes = useStyles();
+
+  const localPrice = Intl.NumberFormat("en-US", {
+    style: "currency", currency: "USD", currencyDisplay: 'symbol'
+  });
+
+  return (
+    <Grid item xs={12} md={6} lg={4}>
+      <ThemeProvider theme={{globalTheme, ...productCardTheme}}>
+        <Card className={classes.productCard}>
+          <CardHeader className={classes.productCardHeader}
+            action={
+              <IconButton 
+                className={classes.productCardButton} 
+                color="warning" 
+                aria-label="add to favourite"
+              >
+                <FavoriteBorderIcon />
+              </IconButton>
+            }
+          />
+          <CardMedia
+            className={classes.productCardMedia}
+            component="img"
+            width="294px"
+            image={`${imageUrls}`}
+            alt={name}
+          />
+          <Rating 
+            className={classes.productCardRating}
+            name="half-rating" defaultValue={2.5} precision={0.5}
+            onChange={f => f}
+          />
+          <CardContent className={classes.productCardContent}>
+            <Typography 
+              className={classes.productCardName} 
+              variant="h3" 
+              color="text.primary"
+            >{name}</Typography>
+            <Typography 
+              className={classes.productCardPrice} 
+              component="span" 
+              variant="h5" 
+              color="text.primary"
+            >{localPrice.format(currentPrice)}</Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton 
+              className={classes.productCardButtonBasket} 
+              aria-label="add to basket" 
+              color="primary"
+            >
+              <ShoppingCartIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </ThemeProvider>
+    </Grid>
+  )
 }
 
 ProductCard.propTypes = {

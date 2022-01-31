7a393d26bfa1dd4a0f78c361e0840b5edcc9a1bc
QmorphQ <90260081+QmorphQ@users.noreply.github.com>
2022-01-31 04:41:23 +0200

    add routs & spinner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+/* import { BrowserRouter, Routes, Route } from "react-router-dom"; */
 import { fetchProducts } from "./store/thunks/products.thunks";
 import fetchCategories from "./store/thunks/catalog.thunks";
 import {
@@ -9,6 +9,7 @@ import {
 } from "./store/selectors/selectors";
 import Home from "./app/pages/Home.jsx";
 import Preloader from "./ui/components/Preloader/Prelodaer.jsx";
+// import AppLayout from "./app/components/AppLayout/AppLayout.jsx"; --MVP - add routes
 
 function App() {
   const downloadRequestState = useSelector(downloadRequestStateSelector);
@@ -26,12 +27,32 @@ function App() {
     return <Preloader />;
   }
   return (
-    <BrowserRouter>
-    <div>
-      <Home loading={downloadRequestState} productList={productList} />
-    </div>
-    </BrowserRouter>
+      <div>
+        <Home loading={downloadRequestState} productList={productList} />
+      </div>
   );
 }
 
+/*
+
+Don't delete!!!. After creating Header & Footer with links as props will be connected. --MVP
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<AllProducts />} />
+          <Route path="description" element={<CurrentProduct />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="about" element={<AboutSeedra />} />
+          <Route path="contact" element={<Page404 />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+*/
 export default App;

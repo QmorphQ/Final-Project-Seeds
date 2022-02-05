7686bdc39bd080bce71b6996c9617f4b1d66ae51
avk.me <avk21.me@gmail.com>
2022-02-05 20:18:39 +0200

    updated productsReducer

--- client/src/store/reducers/products.reducer.js
+++ client/src/store/reducers/products.reducer.js
@@ -2,6 +2,9 @@ import {
   DOWNLOAD_ALL_PRODUCTS_SUCCESS,
   DOWNLOAD_ALL_PRODUCTS_REQUESTED,
   DOWNLOAD_ALL_PRODUCTS_ERROR,
+  DOWNLOAD_FILTERED_PRODUCTS_REQUESTED,
+  DOWNLOAD_FILTERED_PRODUCTS_SUCCESS,
+  DOWNLOAD_FILTERED_PRODUCTS_ERROR,
   FILTER_BY_CATEGORY,
   ADD_PRODUCT_REQUESTED,
   ADD_PRODUCT_SUCCESS,
@@ -13,10 +16,12 @@ import {
 import { downloadRequestStates } from "../../app/constants";
 
 const initialState = {
-  downloadRequestState: downloadRequestStates.IDLE,
+  downloadAllRequestState: downloadRequestStates.IDLE,
+  downloadFilteredRequestState: downloadRequestStates.IDLE,
   addProductRequestState: downloadRequestStates.IDLE,
   productList: [],
   selectedCategories: "all",
+  filteredProducts: [],
 };
 
 const productsReducer = (state = initialState, action) => {
@@ -24,20 +29,39 @@ const productsReducer = (state = initialState, action) => {
     case DOWNLOAD_ALL_PRODUCTS_REQUESTED:
       return {
         ...state,
-        downloadRequestState: downloadRequestStates.LOADING,
+        downloadAllRequestState: downloadRequestStates.LOADING,
       };
 
     case DOWNLOAD_ALL_PRODUCTS_SUCCESS:
       return {
         ...state,
-        downloadRequestState: downloadRequestStates.SUCCESS,
+        downloadAllRequestState: downloadRequestStates.SUCCESS,
         productList: action.payload.data,
       };
 
     case DOWNLOAD_ALL_PRODUCTS_ERROR:
       return {
         ...state,
-        downloadRequestState: downloadRequestStates.ERROR,
+        downloadAllRequestState: downloadRequestStates.ERROR,
+      };
+
+    case DOWNLOAD_FILTERED_PRODUCTS_REQUESTED:
+      return {
+        ...state,
+        downloadFilteredRequestState: downloadRequestStates.LOADING,
+      };
+
+    case DOWNLOAD_FILTERED_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        downloadFilteredRequestState: downloadRequestStates.SUCCESS,
+        filteredProducts: action.payload.data,
+      };
+
+    case DOWNLOAD_FILTERED_PRODUCTS_ERROR:
+      return {
+        ...state,
+        downloadFilteredRequestState: downloadRequestStates.ERROR,
       };
 
     case FILTER_BY_CATEGORY:
@@ -46,43 +70,48 @@ const productsReducer = (state = initialState, action) => {
         selectedCategories: action.payload,
       };
 
-      case ADD_PRODUCT_REQUESTED:
+    case ADD_PRODUCT_REQUESTED:
       return {
         ...state,
         addProductRequestState: downloadRequestStates.LOADING,
       };
 
-      case ADD_PRODUCT_SUCCESS:
+    case ADD_PRODUCT_SUCCESS:
       return {
         ...state,
         addProductRequestState: downloadRequestStates.LOADING,
-        productList: [...state.productList, action.payload.data]
+        productList: [...state.productList, action.payload.data],
       };
 
-      case ADD_PRODUCT_ERROR:
+    case ADD_PRODUCT_ERROR:
       return {
         ...state,
         addProductRequestState: downloadRequestStates.ERROR,
       };
 
-      case UPLOAD_PRODUCT_RATING_REQUESTED:
-        return {
-          ...state,
-          uploadRatingRequestState: downloadRequestStates.LOADING,
-        };
-  
-      case UPLOAD_PRODUCT_RATING_SUCCESS:
-        return {
-          ...state,
-          uploadRatingRequestState: downloadRequestStates.SUCCESS,
-          productList: [...state.productList.filter(product => product.itemNo !== action.payload.data.itemNo), action.payload.data]/* MVP - added state.product */
-        };
-  
-      case UPLOAD_PRODUCT_RATING_ERROR:
-        return {
-          ...state,
-          uploadRatingRequestState: downloadRequestStates.ERROR,
-        };
+    case UPLOAD_PRODUCT_RATING_REQUESTED:
+      return {
+        ...state,
+        uploadRatingRequestState: downloadRequestStates.LOADING,
+      };
+
+    case UPLOAD_PRODUCT_RATING_SUCCESS:
+      return {
+        ...state,
+        uploadRatingRequestState: downloadRequestStates.SUCCESS,
+        productList: [
+          ...state.productList.filter(
+            (product) => product.itemNo !== action.payload.data.itemNo
+          ),
+          action.payload.data,
+        ] /* MVP - added state.product */,
+      };
+
+    case UPLOAD_PRODUCT_RATING_ERROR:
+      return {
+        ...state,
+        uploadRatingRequestState: downloadRequestStates.ERROR,
+      };
 
     default:
       return state;
--- client/src/ui/components/SortBySelect/SortBySelect.jsx
+++ client/src/ui/components/SortBySelect/SortBySelect.jsx
@@ -1,15 +1,12 @@
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
-const SortBySelect = ({ selectedValue, handleChange }) => {
-  return (
-    <FormControl>
-      <InputLabel>Sort by</InputLabel>
-      <Select value={selectedValue} onChange={handleChange}>
-        <MenuItem value={"most"}>Most expensive</MenuItem>
-        <MenuItem value={"less"}>Less expensive</MenuItem>
-      </Select>
-    </FormControl>
-  );
-};
-
+const SortBySelect = ({ selectedValue, handleChange }) => (
+  <FormControl>
+    <InputLabel>Sort by</InputLabel>
+    <Select value={selectedValue} onChange={handleChange}>
+      <MenuItem value={"most"}>Most expensive</MenuItem>
+      <MenuItem value={"less"}>Less expensive</MenuItem>
+    </Select>
+  </FormControl>
+);
 export default SortBySelect;

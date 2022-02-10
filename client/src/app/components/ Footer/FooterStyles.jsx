5f10622fe3aa6fe0351d9530f834fde1ce2e9eaa
QmorphQ <90260081+QmorphQ@users.noreply.github.com>
2022-02-10 14:04:49 +0200

    update

---
+++ client/src/app/components/ Footer/FooterStyles.jsx
@@ -0,0 +1,10 @@
+const classes = {
+    Footer: {
+        position: 'relative',
+        zIndex: 100,
+        border: '1px blue solid',
+    }
+};
+
+// =======================
+export default classes;
---
+++ client/src/app/pages/TestCartPage.jsx
@@ -0,0 +1,5 @@
+export default function TestCartPage(){
+    return(
+        <div style={{width: '300px', height: '300px', backgroundColor: 'teal', margin: 'auto', textAlign: 'center', paddingTop: '100px'}}><h1>CART PAGE</h1></div>
+    )
+}

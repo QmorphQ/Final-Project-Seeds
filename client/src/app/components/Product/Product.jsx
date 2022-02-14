b362fd4073ffc8093f178d282420ac30813eefb9
Snuppiman <98312359+Snuppiman@users.noreply.github.com>
2022-02-14 11:57:13 +0200

    Revert "Product page"

--- client/src/app/components/Product/Product.jsx
+++ client/src/app/components/Product/Product.jsx
@@ -3,8 +3,8 @@ import {ProductCardRender} from "../../../ui/components/ProductCard/ProductCard.
 import { useFetch } from "../../hoc/useFetch.jsx";
 import { API } from "../../constants/index";
 
-const Product = ({id}) => {
-  const [loading, data, error] = useFetch(`${API}products/${id}`);
+const Product = () => {
+  const [loading, data, error] = useFetch(`${API}products/822862`); // MVP change
 
   return (
     <RenderComponent
--- client/src/ui/components/ProductCard/useProductPageStyles.js
+++ client/src/ui/components/ProductCard/useProductPageStyles.js
@@ -18,7 +18,8 @@ export const useProductPageStyles = makeStyles((theme) => ({
     maxHeight:"auto",
   },
   productCardMediaSmall: {
-    borderRadius: "12px",
+    maxWidth: "67px",
+    maxHeight:"auto",
   },
   productCardMedia: {
     borderRadius: "12px",
@@ -36,11 +37,7 @@ export const useProductPageStyles = makeStyles((theme) => ({
       color: `${theme.palette.text.primary}`,
     },
     border: `1px solid ${theme.palette.grey["300"]}`,
-  },
-  productTableInfo: {
-    boxShadow: "none",
-    border: `1px solid ${theme.palette.grey["300"]}`,
-    borderRadius: "12px",
+
   },
   productCardOldPrice: {
     fontSize:"16px",
@@ -59,7 +56,6 @@ export const useProductPageStyles = makeStyles((theme) => ({
     fontSize: "16px",
   },
   amountInputGroup: {
-    margin:"16px",
     height: "44px",
     border: "1px solid rgba(239, 239, 239, 1)",
     borderRadius: "11px",
@@ -68,7 +64,6 @@ export const useProductPageStyles = makeStyles((theme) => ({
     marginRight:"12px"
   },
   productCardButtonBasket: {
-    margin:"16px",
     textTransform: "capitalize"
   },
   customScrollbar: {

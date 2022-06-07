3d10d348a91aaa5118548b1992482e05b1484f53
avk.me <avk21.me@gmail.com>
2022-06-07 21:22:44 +0300

    added possibility to delete product on backend

--- client/src/app/components/Header/HeaderSearch/SearchAppBar.jsx
+++ client/src/app/components/Header/HeaderSearch/SearchAppBar.jsx
@@ -1,14 +1,16 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
 import SearchResultContainer from "./SearchComponent/SearchResultsContainer.jsx";
-import Spinner from '../../../../ui/components/Spinner/Spinner.jsx';
+import Spinner from "../../../../ui/components/Spinner/Spinner.jsx";
 // import SearchBtn from './SearchBtns/SearchBtn.jsx';
 // =========================================================
 import searchDB from "./SearchComponent/SearchLogic/searchDB";
-import searchObserver from './SearchComponent/SearchLogic/searchObserver';
+import searchObserver from "./SearchComponent/SearchLogic/searchObserver";
 import searchNormalize from "./SearchComponent/SearchLogic/searchNormalize";
+import { API } from "../../../constants/index"
 
 // =========================================================
 const Search = styled("div")(({ theme }) => ({
@@ -34,7 +36,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  color: theme.palette.success
+  color: theme.palette.success,
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -54,107 +56,137 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-
 export default function SearchAppBar() {
-// -----------------------------------------------------------------------------------
-// Pressets:
-const [inputText, setInputText] = useState('');
-const [trigger, setTrigger] = useState(false); // Start searching
-const [arrIDs, setIDsArr] = useState([]);
-const [fetchedProducts, setFetchedProducts] = useState([]); // Array of products
-const [loading, setLoading] = useState(false); // Request status
-const [activeSaerchContainer, setActiveSearchContainer] = useState(false); // Render container
-const [ready, setReady] = useState(false);
-// -----------------------------------------------------------------------------------
-// Functions:
-// ++++++
-// Input controller:
-const inputHandler = (event) => {
-  setInputText(searchNormalize(event.target.value));
-}
-// ++++++
-// +++
-const searchContainerHandler = () => {
- 
-   setActiveSearchContainer(false);
-   setTrigger(false);
-   setIDsArr([]);
-   setFetchedProducts([]);
-   setLoading(false);
-   setReady(false);
-   setInputText('');
- 
-}
-// ++++++
-// Get products from server by itemNo:
-const getSearchProducts = async (arrOfProductsItemNo = []) => {
-  arrOfProductsItemNo.map(async (itemNo) => {
-await fetch(`http://localhost:8000/api/products/${itemNo}`).
- then((r) => r.json()).
- then((r) => setFetchedProducts((val) => val.find((prod) => prod.itemNo === r.itemNo) ? val : [...val, r])).
- catch(error => console.log(error.message))
-})
-setLoading(false);
-setReady(true);
-};
-// ++++++
-// -----------------------------------------------------------------------------------
-// Hooks:
-// ++++++
-// Start of searching process:
-useEffect(() => {
-  if (!trigger && searchObserver(inputText)) {
-    // Clear previous results:
-    setFetchedProducts([]);
-    setIDsArr([]);
+  // -----------------------------------------------------------------------------------
+  // Pressets:
+  const [inputText, setInputText] = useState("");
+  const [trigger, setTrigger] = useState(false); // Start searching
+  const [arrIDs, setIDsArr] = useState([]);
+  const [fetchedProducts, setFetchedProducts] = useState([]); // Array of products
+  const [loading, setLoading] = useState(false); // Request status
+  const [activeSaerchContainer, setActiveSearchContainer] = useState(false); // Render container
+  const [ready, setReady] = useState(false);
+  // -----------------------------------------------------------------------------------
+  // Functions:
+  // ++++++
+  // Input controller:
+  const inputHandler = (event) => {
+    setInputText(searchNormalize(event.target.value));
+  };
+  // ++++++
+  // +++
+  const searchContainerHandler = () => {
     setActiveSearchContainer(false);
-    return setTrigger(true)}
-  return false;
-}, [inputText]);
-// ++++++
-// Loading:
-useEffect(() => {
-  if (trigger) {
-    const regEx = new RegExp(`${inputText.trim()}`,'gi')
-    const filteredProducts = Array.from(new Set((searchDB.filter((prod) => regEx.test(prod.name))).map((prod) => prod.itemNo)));
-    return filteredProducts.length > 0 ? setIDsArr([...filteredProducts]) : [console.log('no results'), setTrigger(false)]};
-    return false
-}, [trigger]);
-// ++++++
-// Fetch products from server:
-useEffect(() => {
-  if (arrIDs.length > 0) {
-    setLoading(true);
-    getSearchProducts(arrIDs);
-  }
-}, [arrIDs])
-// ++++++
-// Rendering results:
-useEffect(() => {
-  if (ready === true) {
-    setActiveSearchContainer(true);
-  } return [setTrigger(false), setReady(false)];
-}, [ready])
-useEffect(() => {
-  document.addEventListener('click', searchContainerHandler);
-  return () => document.removeEventListener('click', searchContainerHandler);
-}, [])
-// ---------------------------------------------------------------------------------
-return (
-  <>
-    <Search component='input' sx={{ border: "solid rgba(0, 0, 0, 0.2) 1px", borderRadius: 20}}>
-      <SearchIconWrapper><SearchIcon /></SearchIconWrapper>
-      <StyledInputBase
-      required
-        value={inputText}
-        placeholder='search'
-        onChange={inputHandler}
-      />{loading ? <Spinner left={'70%'} top={'22%'}/> : false}
-      {<SearchResultContainer active={activeSaerchContainer} products={fetchedProducts} oneCard={fetchedProducts.length === 1}/>}
-    </Search>
-    
+    setTrigger(false);
+    setIDsArr([]);
+    setFetchedProducts([]);
+    setLoading(false);
+    setReady(false);
+    setInputText("");
+  };
+  // ++++++
+
+  // const [searchKeys, setSearchKeys] = useState([]);
+  // Get products from server by itemNo:
+  const getSearchProducts = async (arrOfProductsItemNo = []) => {
+    arrOfProductsItemNo.map(async (itemNo) => {
+      await fetch(`http://localhost:8000/api/products/${itemNo}`)
+        .then((r) => r.json())
+        .then((r) =>
+          setFetchedProducts((val) =>
+            val.find((prod) => prod.itemNo === r.itemNo) ? val : [...val, r]
+          )
+        )
+        .catch((error) => console.log(error.message));
+    });
+    setLoading(false);
+    setReady(true);
+  };
+  // ++++++
+  // -----------------------------------------------------------------------------------
+  // Hooks:
+  // ++++++
+  // Start of searching process:
+  useEffect(() => {
+    if (!trigger && searchObserver(inputText)) {
+      // Clear previous results:
+      setFetchedProducts([]);
+      setIDsArr([]);
+      setActiveSearchContainer(false);
+      return setTrigger(true);
+    }
+    return false;
+  }, [inputText]);
+  // ++++++
+  // Loading:
+  useEffect(() => {
+    if (trigger) {
+      const regEx = new RegExp(`${inputText.trim()}`, "gi");
+      const filteredProducts = Array.from(
+        new Set(
+          searchDB
+            .filter((prod) => regEx.test(prod.name))
+            .map((prod) => prod.itemNo)
+        )
+      );
+      return filteredProducts.length > 0
+        ? setIDsArr([...filteredProducts])
+        : [console.log("no results"), setTrigger(false)];
+    }
+    return false;
+  }, [trigger]);
+  // ++++++
+  // Fetch products from server:
+  useEffect(() => {
+    if (arrIDs.length > 0) {
+      setLoading(true);
+      getSearchProducts(arrIDs);
+    }
+  }, [arrIDs]);
+  // ++++++
+  // Rendering results:
+  useEffect(() => {
+    if (ready === true) {
+      setActiveSearchContainer(true);
+    }
+    return [setTrigger(false), setReady(false)];
+  }, [ready]);
+  
+  useEffect(() => {
+    document.addEventListener("click", searchContainerHandler);
+    return () => document.removeEventListener("click", searchContainerHandler);
+  }, []);
+
+  useEffect(async () => {
+    const keys = await axios.get(`${API}searchKeys`);
+   console.log(keys.data)
+  
+  }, []);
+  // ---------------------------------------------------------------------------------
+  return (
+    <>
+      <Search
+        component="input"
+        sx={{ border: "solid rgba(0, 0, 0, 0.2) 1px", borderRadius: 20 }}
+      >
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          required
+          value={inputText}
+          placeholder="search"
+          onChange={inputHandler}
+        />
+        {loading ? <Spinner left={"70%"} top={"22%"} /> : false}
+        {
+          <SearchResultContainer
+            active={activeSaerchContainer}
+            products={fetchedProducts}
+            oneCard={fetchedProducts.length === 1}
+          />
+        }
+      </Search>
     </>
   );
 }
-
-
--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const shippingMethods = require("./routes/shippingMethods");
 const paymentMethods = require("./routes/paymentMethods");
 const partners = require("./routes/partners");
 const mainRoute = require("./routes/index");
+const searchKeys = require("./routes/searchKeys");
 
 const app = express();
 
@@ -69,6 +70,7 @@ app.use("/api/shipping-methods", shippingMethods);
 app.use("/api/payment-methods", paymentMethods);
 app.use("/api/partners", partners);
 app.use("/", mainRoute);
+app.use("/api/searchKeys", searchKeys);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {

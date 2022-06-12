3a69de2aced700635a33ac3a4667065ed7e75001
Igor Rakhil <hq@pilot-logistic.eu>
2022-06-12 15:32:22 +0300

    ready adaptive footer

--- client/src/app/components/Footer/useFooterStyles.jsx
+++ client/src/app/components/Footer/useFooterStyles.jsx
@@ -1,70 +1,59 @@
 import { makeStyles } from "@mui/styles";
 
 export const useFooterStyles = makeStyles(() => ({
-  LogoUp: {
-    margin: "auto",
-    display: "table",
-    paddingBottom: "37px",
+  Wrapper: {
+    color: "#70737C",
+    fontSize: "14px",
   },
-
   BoxTop: {
+    justifyContent: "center",
+    padding: "36px 0px",
+    color: "#70737C",
+  },
+
+  BoxMain: {
     display: "flex",
-    marginBottom: "25px",
-    flexWrap: "nowrap",
-    alignItems: "center",
-  },
-
-  GridContainer: {
-    flexWrap: "wrap",
-    flexDirection: "row",
-    alignContent: "space-around",
     justifyContent: "space-evenly",
     alignItems: "center",
-    // paddingBottom: "24px",
   },
 
-  GridContainerColumn: {
-    flexWrap: "wrap",
-    flexDirection: "column",
-    alignContent: "space-around",
-    justifyContent: "space-evenly",
+  BoxLink: {
+    display: "flex",
     alignItems: "center",
-    // paddingBottom: "24px",
-  },
-
-  TypogLink: {
-    display: "inlineBlock",
-
+    flexGrow: "1",
+    justifyContent: "space-evenly",
   },
 
   Link: {
+    padding: "12px 1px 12px 10px",
     textDecoration: "none",
-    marginBottom: "24px",
-    fontSize: 14,
-    color: "#70737C",
-    lineHeight: "36px",
+    color: "inherit",
+    fontSize: "inherit",
+    "&:hover": {
+      padding: "12px 0px 12px 9px",
+      color: "#359740;",
+      fontWeight: "500",
+    },
+    "&:active": {
+      color: "#359740;",
+      fontWeight: "500",
+    },
   },
 
   DividerVertical: {
     height: "25px",
-    color: "#4FA083",
+    backgroundColor: "#4FA083",
   },
 
   BoxBottom: {
     display: "flex",
-    paddingLeft: "5%",
-    paddingRight: "5%",
+    padding: "22px 0px",
     justifyContent: "space-between",
     alignItems: "center",
   },
 
   FooterText: {
     component: "span",
-    fontSize: "14px",
-    color: "#70737C",
+    fontSize: "inherit",
   },
-
-  // LogoBox: {
-  //         order: -1,
-  // }
 }));

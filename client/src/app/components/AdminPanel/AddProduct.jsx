0f5116cabb2d6b1e2aa124943f7d2ec5df8cad39
franchuk.nik <franchuk.nik@gmail.com>
2022-06-16 02:56:46 +0300

    admin feature: update product

--- client/src/app/components/AdminPanel/AddProduct.jsx
+++ client/src/app/components/AdminPanel/AddProduct.jsx
@@ -1,15 +1,19 @@
 
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; 
+import PropTypes from 'prop-types';
 import { useFormik } from 'formik';
 import * as yup from 'yup'; 
-import { useSelector, useDispatch } from 'react-redux'; 
 
-import { MenuItem, TextField, Button } from '@mui/material'; 
+import { MenuItem, TextField, Button, CardMedia, IconButton } from '@mui/material'; 
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'; 
+import AddIcon from '@mui/icons-material/Add';
 
-import { productCategories } from '../../constants';  
+import { productCategories } from '../../constants'; 
 
 import { adminAddProductRequestSelector } from '../../../store/selectors/selectors';
 import { adminAddProductIdle } from '../../../store/actions/admin.actions';
-import { adminAddProduct } from '../../../store/thunks/admin.thunks';
+import { adminAddProduct, adminUpdateProduct } from '../../../store/thunks/admin.thunks'; 
 
 
 
@@ -51,55 +55,83 @@ const validationSchema = yup.object({
         .required('required') 
         .max(4, 'too long value')
         .matches(/[0-9]/, 'value is not valid, please, enter a decimal number'),
+});
 
-    imageUrls: yup
-        .string()
-        .required('required'), 
-
-  });
+  
 
 
-
-
-const AddProduct = () => {
+const AddProduct = ({ product, onClose }) => {
 
     const isProductAdded = useSelector(adminAddProductRequestSelector);
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(); 
 
-    
+    const [restAfterDeleteImg, setRestAfterDeleteImg] = useState(product?.imageUrls)
+    const [fieldsList, setFieldsList] = useState([0]);
+
+
+    const handleDeleteImage = (e) => {
+        setRestAfterDeleteImg(restAfterDeleteImg.filter(item => item !== e.currentTarget.dataset.url)); 
+        e.target.closest('.prod_icon').remove();
+    } 
+
+    const handleAddField = () => {
+        if (fieldsList.length <= 4) {
+            setFieldsList([...fieldsList, fieldsList.length]);
+        }
+    };
+
+
 
     const formik = useFormik({
         initialValues: {
-            name: '', 
-            description: '', 
-            categories: '', 
-            currentPrice: '', 
-            discountPrice: '', 
-            packageDimensions: '', 
-            currentRating: '', 
-            quantity: '', 
-            imageUrls: '', 
+            name: product?.name || '', 
+            description: product?.description || '', 
+            categories: product?.categories || '', 
+            currentPrice: product?.currentPrice || '', 
+            discountPrice: product?.discountPrice || '', 
+            packageDimensions: product?.packageDimensions || '', 
+            currentRating: product?.currentRating || '', 
+            quantity: product?.quantity || '', 
+            imageUrls0: '', 
+            imageUrls1: '',
+            imageUrls2: '',
+            imageUrls3: '',
+            imageUrls4: '',
         }, 
 
         validationSchema, 
 
         onSubmit: (values) => { 
-            
-            dispatch(adminAddProduct({
-                                        name: values.name, 
-                                        description: values.description, 
-                                        categories: values.categories, 
-                                        currentPrice: Number(values.currentPrice), 
-                                        discountPrice: Number(values.discountPrice), 
-                                        packageDimensions: values.packageDimensions, 
-                                        currentRating: Number(values.currentRating), 
-                                        quantity: Number(values.quantity), 
-                                        imageUrls: values.imageUrls, 
-                                    })); 
-            formik.resetForm(); 
-        }
-    });
 
+            const newAddedImg = [values.imageUrls0, 
+                                 values.imageUrls1,
+                                 values.imageUrls2,
+                                 values.imageUrls3,
+                                 values.imageUrls4].filter(item => item !== ''); 
+ 
+            const payload = { name: values.name, 
+                              description: values.description, 
+                              categories: values.categories, 
+                              currentPrice: Number(values.currentPrice), 
+                              discountPrice: Number(values.discountPrice), 
+                              packageDimensions: values.packageDimensions, 
+                              currentRating: Number(values.currentRating), 
+                              quantity: Number(values.quantity) };
+
+            if (product) {
+
+                const resultImgList = [...newAddedImg, ...restAfterDeleteImg]; 
+                 
+                dispatch(adminUpdateProduct(product._id, { ...payload, imageUrls: resultImgList })); 
+                onClose(); 
+
+            } else { 
+                
+                dispatch(adminAddProduct({ ...payload, imageUrls: newAddedImg })); 
+                formik.resetForm();
+            }  
+        }
+    }); 
 
 
     return (
@@ -114,8 +146,9 @@ const AddProduct = () => {
                             width: '100%', }}>
 
                         <div style={{ margin: '3% 0',
-                                    display: 'flex', 
-                                    flexFlow: 'column wrap' }}>
+                                      display: 'flex', 
+                                      flexFlow: 'column wrap' }}> 
+
                             <TextField 
                                 name='name'
                                 id='outlined-basic' 
@@ -123,7 +156,7 @@ const AddProduct = () => {
                                 variant='outlined' 
                                 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.name}
                                 onChange={formik.handleChange}
                                 error={formik.touched.name && Boolean(formik.errors.name)}
@@ -137,7 +170,7 @@ const AddProduct = () => {
                                 variant='outlined' 
                                 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.description}
                                 onChange={formik.handleChange}
                                 error={formik.touched.description && Boolean(formik.errors.description)}
@@ -149,7 +182,7 @@ const AddProduct = () => {
                                 name='categories'
                                 select
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 label='categories'
                                 value={formik.values.categories}
                                 onChange={formik.handleChange}
@@ -166,14 +199,15 @@ const AddProduct = () => {
 
                         <div style={{ margin: '3% 0',
                                     display: 'flex', 
-                                    flexFlow: 'column wrap' }}>
+                                    flexFlow: 'column wrap' }}> 
+
                             <TextField 
                                 name='currentPrice'
                                 id='outlined-basic' 
-                                label='currentPrice' 
+                                label='current price' 
                                 variant='outlined' 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.currentPrice}
                                 onChange={formik.handleChange}
                                 error={formik.touched.currentPrice && Boolean(formik.errors.currentPrice)}
@@ -183,10 +217,10 @@ const AddProduct = () => {
                             <TextField 
                                 name='discountPrice'
                                 id='outlined-basic' 
-                                label='discountPrice (optional)' 
+                                label='discount price (optional)' 
                                 variant='outlined' 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.discountPrice} 
                                 onChange={formik.handleChange}
                                 error={formik.touched.discountPrice && Boolean(formik.errors.discountPrice)}
@@ -196,10 +230,10 @@ const AddProduct = () => {
                             <TextField 
                                 name='currentRating'
                                 id='outlined-basic' 
-                                label='currentRating (optional)' 
+                                label='current rating (optional)' 
                                 variant='outlined' 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.currentRating} 
                                 onChange={formik.handleChange}
                                 error={formik.touched.currentRating && Boolean(formik.errors.currentRating)}
@@ -209,16 +243,16 @@ const AddProduct = () => {
 
 
                         <div style={{ margin: '3% 0',
-                                    display: 'flex', 
-                                    flexFlow: 'column wrap' }}>
+                                      display: 'flex', 
+                                      flexFlow: 'column wrap' }}>
 
-                        <TextField 
+                            <TextField 
                                 name='quantity'
                                 id='outlined-basic' 
                                 label='quantity' 
                                 variant='outlined' 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.quantity} 
                                 onChange={formik.handleChange}
                                 error={formik.touched.quantity && Boolean(formik.errors.quantity)}
@@ -231,31 +265,81 @@ const AddProduct = () => {
                                 label='package dimensions' 
                                 variant='outlined' 
                                 sx={{ width: 350, 
-                                        m: '10px' }}
+                                      m: '10px' }}
                                 value={formik.values.packageDimensions} 
                                 onChange={formik.handleChange}
                                 error={formik.touched.packageDimensions && Boolean(formik.errors.packageDimensions)}
                                 helperText={formik.touched.packageDimensions && formik.errors.packageDimensions}
-                            />
-
-                            <TextField 
-                                name='imageUrls'
-                                id='outlined-basic' 
-                                label='image URL' 
-                                variant='outlined' 
-                                sx={{ width: 350, 
-                                        m: '10px' }}
-                                value={formik.values.imageUrls} 
-                                onChange={formik.handleChange}
-                                error={formik.touched.imageUrls && Boolean(formik.errors.imageUrls)}
-                                helperText={formik.touched.imageUrls && formik.errors.imageUrls}
                             /> 
+
+                            <div style={{ display: 'flex', 
+                                          flexFlow: 'row wrap', 
+                                          margin: '30px 0'}}>
+
+                            {product && product.imageUrls.map((item, index) => 
+                                <div key={`${index}`} 
+                                     style={{ position: 'relative'}}
+                                     className='prod_icon'> 
+
+                                    <CardMedia 
+                                        component='img' 
+                                        image={item}
+                                        sx={{ width: '67px', 
+                                              m: '10px', 
+                                              borderRadius: '12px', }} 
+                                    /> 
+                                    <IconButton 
+                                        disableRipple={true}
+                                        data-url={item}
+                                        onClick={handleDeleteImage}>
+                                            <HighlightOffIcon 
+                                                sx={{ color: 'red', 
+                                                      position: 'absolute', 
+                                                      bottom: '80px',
+                                                      left: '65px',}} /> 
+                                    </IconButton>
+
+                                </div>
+                                )
+                            } 
+                            </div>
+
+
+                            {fieldsList.map((field) => (
+                                <div key={field} 
+                                     style={{ display: 'flex', 
+                                              flexFlow: 'row nowrap', }}>
+
+                                    <TextField 
+                                        name={`imageUrls${field}`}
+                                        id='outlined-basic' 
+                                        label={ product ? `add new image URL` : `image URL (optional)` }
+                                        variant='outlined' 
+                                        sx={{ width: 350, 
+                                              m: '10px 5px 10px 10px' }}
+                                        value={formik.values[`imageUrls${field}`]} 
+                                        onChange={formik.handleChange}
+                                    /> 
+
+                                    <IconButton 
+                                        disableRipple={true}
+                                        onClick={handleAddField}
+                                    >
+                                        <AddIcon 
+                                            sx={{ color: '#50a257' }} 
+                                        />
+                                    </IconButton> 
+
+                                </div>
+                            ))}
+
+
                         </div>
                     </div>
 
-                    <div style={{ margin: '2% 10px 10%',  
-                                display: 'flex', 
-                                flexDirection: 'row' }}>
+                    <div style={{ margin: '30px 10px 10%',  
+                                  display: 'flex', 
+                                  flexDirection: 'row' }}>
                         <Button 
                             size='large' 
                             color='success' 
@@ -271,7 +355,7 @@ const AddProduct = () => {
                                 }
                             }}
                         >
-                            Add Product
+                            { product ? `Update Product` : `Add Product` }
                         </Button>
                     </div>
                 </form>
@@ -287,23 +371,22 @@ const AddProduct = () => {
                         variant='contained' 
                         onClick={() => dispatch(adminAddProductIdle())}
                         sx={{
-                        width: 220, 
-                        backgroundColor: '#50a257', 
-                        boxShadow: 1, 
-                        color: 'white', 
-                        ':hover': {
-                            backgroundColor: '#50a257',
-                        }
+                            width: 220, 
+                            backgroundColor: '#50a257', 
+                            boxShadow: 1, 
+                            color: 'white', 
+                            ':hover': {
+                                backgroundColor: '#50a257',
+                            }
                         }}
                     >
                         Add One More
                     </Button> 
 
-                
                     <span style={{ margin: '10px 0 0 30px', 
-                            color: '#50a257', 
-                            fontFamily: "'Lexend', sans-serif", }}>
-                                    product has been added successfully
+                                   color: '#50a257', 
+                                   fontFamily: "'Lexend', sans-serif", }}>
+                                        product has been added successfully
                     </span>
                 </div>
             }
@@ -312,3 +395,9 @@ const AddProduct = () => {
 }
 
 export default AddProduct; 
+
+
+AddProduct.propTypes = {
+    product: PropTypes.object, 
+    onClose: PropTypes.func, 
+};
--- client/src/store/actions/admin.actions.js
+++ client/src/store/actions/admin.actions.js
@@ -41,3 +41,26 @@ export const ADMIN_DELETE_PRODUCT_ERROR = "ADMIN_DELETE_PRODUCT_ERROR";
 export const adminDeleteProductError = () => ({
   type: ADMIN_DELETE_PRODUCT_ERROR,
 });
+
+
+
+
+export const ADMIN_UPDATE_PRODUCT_IDLE = "ADMIN_UPDATE_PRODUCT_IDLE";
+export const adminUpdateProductIdle = () => ({
+  type: ADMIN_UPDATE_PRODUCT_IDLE,
+});
+
+export const ADMIN_UPDATE_PRODUCT_REQUESTED = "ADMIN_UPDATE_PRODUCT_REQUESTED";
+export const adminUpdateProductRequested = () => ({
+  type: ADMIN_UPDATE_PRODUCT_REQUESTED,
+});
+
+export const ADMIN_UPDATE_PRODUCT_SUCCESS = "ADMIN_UPDATE_PRODUCT_SUCCESS";
+export const adminUpdateProductSuccess = () => ({ 
+  type: ADMIN_UPDATE_PRODUCT_SUCCESS, 
+});
+
+export const ADMIN_UPDATE_PRODUCT_ERROR = "ADMIN_UPDATE_PRODUCT_ERROR";
+export const adminUpdateProductError = () => ({
+  type: ADMIN_UPDATE_PRODUCT_ERROR,
+});

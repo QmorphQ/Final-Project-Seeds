643ca1e0e44698c0134643f6226015821cc630fb
Сергей Примаков <sergogo755@gmail.com>
2022-06-16 20:44:36 +0300

    fix errorHandler

--- client/src/app/hoc/RequireAuth.jsx
+++ client/src/app/hoc/RequireAuth.jsx
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 
 const RequireAuth = ({children}) => {
     const auth = localStorage.getItem("jwt")
-    return !auth ? <> <Navigate to='/'/> </> : children
+    return !auth ? <> <Navigate to='/login'/> </> : children
 }
 
 export {RequireAuth}

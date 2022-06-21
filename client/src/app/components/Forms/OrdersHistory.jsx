c076fb37ac9d6e3ff78518419bcb38092f44c74c
Сергей Примаков <sergogo755@gmail.com>
2022-06-21 15:29:09 +0300

    empty orders hisory add

--- client/src/app/components/Forms/OrdersHistory.jsx
+++ client/src/app/components/Forms/OrdersHistory.jsx
@@ -1,32 +1,45 @@
 import { useEffect } from "react"
+import { Typography, Container } from "@mui/material";
+// import { makeStyles } from "@mui/styles";
 import { useDispatch, useSelector } from "react-redux"
 import { customerOrdersHistory } from "../../../store/selectors/selectors"
 import { getOrders } from "../../../store/thunks/customer.thunks"
 import Preloader from "../../../ui/components/Preloader/Preloader.jsx"
 
 
-
 export default function OrdersHistory() {
     const dispatch = useDispatch()
     const ordersList = useSelector(customerOrdersHistory)
     console.log(ordersList);
+
     const getCustomerOrders = orders => {
-        dispatch(getOrders(orders))
-        
+        dispatch(getOrders(orders))      
     }
 
     useEffect(() => {
       getCustomerOrders()
       },[])
 
+    if(ordersList?.length === 0){
+      return (
+        <Container>
+          <Typography
+            variant="h2"
+            component="h2"
+            sx={{ textAlign: "center" }}
+          >
+            No Orders yet
+          </Typography>
+        </Container>
+      );
+    }
 
     return(
       <>
       {ordersList === undefined ? <Preloader/> : ordersList.map((order, index) => 
-          <div key={index}>{order?.email}</div>
+         <div key={index}>{order?.email}</div>
         )}
       
-
       </>
     )
 }

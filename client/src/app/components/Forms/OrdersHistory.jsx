5593979660856be5816de337065bc0696048e5c9
Сергей Примаков <sergogo755@gmail.com>
2022-06-22 15:44:37 +0300

    order history 1/3

--- client/src/app/components/Forms/OrdersHistory.jsx
+++ client/src/app/components/Forms/OrdersHistory.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Grid } from "@mui/material";
 // import { makeStyles } from "@mui/styles";
 import { useDispatch, useSelector } from "react-redux"
 import { customerOrdersHistory } from "../../../store/selectors/selectors"
@@ -35,11 +35,23 @@ export default function OrdersHistory() {
     }
 
     return(
-      <>
-      {ordersList === undefined ? <Preloader/> : ordersList.map((order, index) => 
-         <div key={index}>{order?.email}</div>
-        )}
+      <Grid style={{maxWidth:1150,margin:"30px auto"}} container direction="column" columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {ordersList === undefined ? <Preloader/> : ordersList.map((order, index) => 
+          <Grid item style={{border:"2px solid green", borderRadius:"5px", padding:10, margin:10, height:"auto"}} key={index}>
+            <Typography variant="h6">OrderNo: {order?.orderNo}</Typography>
+              <Grid style={{paddingTop:10}} container justifyContent="space-between" columns={{ xs: 6, md: 12 }}>
+                <Grid justifyContent="flex-start" item>
+                {order.products.map((product,key) =>
+                  <Grid style={{paddingBottom:10}}  container key={key}><img style={{float:"left"}} align="top" width={70} src={product.product.imageUrls[0]} alt="/" /><Typography style={{paddingLeft:10}}> {product.product.name}</Typography></Grid>
+                )}
+                </Grid>
+                <Grid justifyContent="flex-end" item>
+                {order.mobile}
+                </Grid>
+              </Grid>
+          </Grid>
+          )}
       
-      </>
+      </Grid>
     )
 }
